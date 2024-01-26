78b6cec81d6079841e606fddeddf8674a118aa02
Jiwon Kim <lost731@naver.com>
2024-01-26 17:52:14 +0900

    feat(ts-codegen): make object value type property not optional

--- ts-codegen/undefinedCheck.ts
+++ ts-codegen/undefinedCheck.ts
@@ -7,7 +7,7 @@ export const modifyNullableDescriptionToUndefinedType = (
     if (node.getKind() === SyntaxKind.PropertySignature) {
       const property = node.asKind(SyntaxKind.PropertySignature);
       if (property) {
-        if (!property.getLeadingCommentRanges().length) return;
+        // if (!property.getLeadingCommentRanges().length) return;
         const comment = property.getLeadingCommentRanges()[0].getText() || '';
         // 주석에 'nullable' 이 포함된 경우에만 처리
         if (!comment.includes('nullable')) {
