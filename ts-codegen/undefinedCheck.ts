8af298e475a3a4b1fff299671856c76ce01e5ada
Jiwon Kim <lost731@naver.com>
2024-01-27 10:09:13 +0900

    feat(ts-codegen): modify undefined check

--- ts-codegen/undefinedCheck.ts
+++ ts-codegen/undefinedCheck.ts
@@ -8,7 +8,8 @@ export const modifyNullableDescriptionToUndefinedType = (
       const property = node.asKind(SyntaxKind.PropertySignature);
       if (property) {
         // if (!property.getLeadingCommentRanges().length) return;
-        const comment = property.getLeadingCommentRanges()[0].getText() || '';
+        const comment =
+          property.getLeadingCommentRanges()?.[0]?.getText() || '';
         // 주석에 'nullable' 이 포함된 경우에만 처리
         if (!comment.includes('nullable')) {
           property.setHasQuestionToken(false);
